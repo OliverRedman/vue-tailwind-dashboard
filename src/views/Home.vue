0815fe52dc14fcca7d926fef6ea1e22b64ddb919
@OliverRedman <oliredman@hotmail.com>
2021-04-04 12:57:37 +0100

    start

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section
+    class="flex justify-center items-center bg-blue-50 h-full w-full flex-wrap "
+  >
+    <div
+      class="bg-red-200 w-80 h-52 rounded-md text-lg text-center flex flex-col justify-center m-5"
+    >
+      <p class="self-center">hello</p>
+    </div>
+    <div
+      class="bg-red-200 w-80 h-52 rounded-md text-lg text-center flex flex-col justify-center m-5"
+    >
+      <p class="self-center">hello</p>
+    </div>
+    <div
+      class="bg-red-200 w-80 h-52 rounded-md text-lg text-center flex flex-col justify-center m-5"
+    >
+      <p class="self-center">hello</p>
+    </div>
+    <div
+      class="bg-red-200 w-80 h-52 rounded-md text-lg text-center flex flex-col justify-center m-5"
+    >
+      <p class="self-center">hello</p>
+    </div>
+  </section>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
+
+<style></style>
